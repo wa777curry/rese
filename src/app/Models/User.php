7393bcf43fb08c3b0e5a5daf59089021cb117107
ww <ariko_117@hotmail.com>
2024-01-16 02:19:42 +0900

    微調整

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,25 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable as FortifyUser;
 
 class User extends Authenticatable
 {
-    use HasFactory;
-    // Notifiable, TwoFactorAuthenticatable; // 二段階認証
-
     protected $fillable = ['username', 'email', 'password'];
-
-    // protected $hidden = [
-    //    'password',
-    //    'remember_token',
-    //    'two_factor_recovery_codes',
-    //    'two_factor_secret',
-    //];
 
     public function shops() {
         return $this->belongsToMany(Shop::class, 'reservations')
